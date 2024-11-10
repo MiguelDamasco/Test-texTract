1816d27fa22c7ba7714d38d79c127f1b9182df26
[Miguel] <11damascomiguel@gmail.com>
2024-11-09 23:47:59 -0300

    CORS politica actualizada

--- demo/src/main/java/com/example/demo/WebConfig.java
+++ demo/src/main/java/com/example/demo/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica CORS a todos los endpoints
-                .allowedOrigins("http://127.0.0.1:5500") // Permite solicitudes desde este origen
+                .allowedOrigins("http://127.0.0.1:5500", "http://mi-pagina-web-froggstar.s3-website.us-east-2.amazonaws.com/") // Permite solicitudes desde este origen
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos los headers
                 .allowCredentials(true); // Permite cookies o credenciales
